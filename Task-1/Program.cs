00083d839f0a988c6eed5375eea25dca8c2dc86a
biome <biome@DESKTOP-AISV54F>
2022-06-18 12:28:52 +0700

    Добавьте файлы проекта.

---
+++ Task-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static string[] WordSeparation(string s)
+        {
+            string[] separator = s.Split(' ');
+
+            foreach(string word in separator)
+            {
+                Console.WriteLine(word);
+            }
+            return separator;
+
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            WordSeparation(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static string[] WordSeparaiton(string text)
+        {
+            string[] separator = text.Split(' ');
+            return separator;
+        }
+        static string ReverseWord(string[] text)
+        {
+            
+            Array.Reverse(text);
+            string result = string.Join(" ", text);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var met = WordSeparaiton(s);
+
+            s = ReverseWord(met);
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
